b7b17cac01535e4e4db291789353711e5909ce97
Markos <markosmk@me.com>
2022-02-05 17:17:56 -0300

    update: added reduce for calculation Total + Items

--- 1-practice-cart/js/cartClass.js
+++ 1-practice-cart/js/cartClass.js
@@ -19,6 +19,8 @@ class Cart {
     // chekeamos si el producto existe
     const item = products.find((item) => item.id === Number(id));
     if (item) {
+      //const newItem = Object.assign(item, { cant: 1 });
+      //this.list.push(newItem); {
       this.list.push({ ...item, cant: 1 });
     }
     this.updateChanges();
@@ -26,13 +28,12 @@ class Cart {
 
   updateQuantityItem(id, cant) {
     let cartItems = this.list.map((item) => {
-      let newCant = item.cant;
       if (item.id === Number(id)) {
         if (cant >= 1 && cant <= item.stock) {
-          newCant = Number(cant);
+          item.cant = Number(cant);
         }
       }
-      return { ...item, cant: newCant };
+      return item;
     });
 
     this.updateChanges(cartItems);
@@ -44,15 +45,51 @@ class Cart {
   }
 
   calculateTotal() {
-    let totalPrice = 0,
-      totalItems = 0;
-    // TODO: intentar hacerlo mas simple con reduce()
+    // 1) Opcion con forEach
+    /*
+    let totalPrice = 0 = totalItems = 0;
     this.list.forEach((item) => {
       totalPrice += item.precio * item.cant;
       totalItems += item.cant;
     });
-    this.numberItems = totalItems;
     this.total = totalPrice;
+    this.numberItems = totalItems;
+
+    // 2) Opcion con Doble Reduce
+    // para obtener el precio total
+    this.total = this.list.reduce((item, val) => {
+      return item + (val.precio * val.cant);
+    }, 0);
+    // para obtener el total de items
+    this.numberItems = this.list.reduce((item, val) => {
+      return item.cant + val.cant;
+    });
+  */
+    // 3) Opcion con 1 reduce -> return { total: 000.00, items: 00 }
+    let amount = this.list.reduce(
+      function (item, val) {
+        item.total = item.total + val.precio * val.cant;
+        item.items = item.items + val.cant;
+        return item;
+      },
+      { total: 0, items: 0 }
+    );
+    this.total = amount.total;
+    this.numberItems = amount.items;
+
+    /*
+    // 4) Opcion con 1 reduce -> return [ totalPrice, totalItems]
+    let amount = this.list.reduce(
+      function (item, val) {
+        item[0] = item[0] + val.precio * val.cant;
+        item[1] = item[1] + val.cant;
+        return item;
+      },
+      [0, 0]
+    );
+    this.total = amount[0];
+    this.numberItems = amount[1];
+   */
   }
 
   updateChanges(newList = null) {

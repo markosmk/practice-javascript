e4192a41766dfc47ba24b5953d9e583ad26735f1
Markos <markosmk@me.com>
2022-02-05 11:43:53 -0300

    add: list products

---
+++ 1-practice-cart/js/products.js
@@ -0,0 +1,12 @@
+const products = [
+  { id: 1, nombre: 'ensalada cesar', precio: 250, stock: 5 },
+  { id: 2, nombre: 'ensalada mix de hojas verdes', precio: 180, stock: 5 },
+  { id: 3, nombre: 'ensalada mixta pollo', precio: 230, stock: 23 },
+  { id: 4, nombre: 'ensalada mixta con lomito de atun', precio: 250, stock: 12 },
+  { id: 5, nombre: 'ensalada mixta con camarones', precio: 315, stock: 22 },
+  { id: 6, nombre: 'ensalada mixta con salmon', precio: 300, stock: 12 },
+  { id: 7, nombre: 'ensalada tomate', precio: 240, stock: 6 },
+  { id: 8, nombre: 'ensalada palta y mango', precio: 300, stock: 8 },
+  { id: 9, nombre: 'ensalada rusa', precio: 250, stock: 23 },
+  { id: 10, nombre: 'ensalada waldorf', precio: 290, stock: 12 },
+];
